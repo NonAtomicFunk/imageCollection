7d6891b0863a00449d606e9def089f6f1d413c33
admin2 <admin2@macbook-pro-admin2.local>
2018-12-20 13:29:09 +0200

    Initial Commit

---
+++ imageCollection/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  imageCollection
+//
+//  Created by admin2 on 12/20/18.
+//  Copyright © 2018 admin2. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
