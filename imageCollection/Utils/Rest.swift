fc17829ad6e4e4cf3ffb8a84aacad2d198b91c27
admin2 <admin2@macbook-pro-admin2.local>
2018-12-27 12:40:33 +0200

    obsoletes and comments removed

--- imageCollection/Utils/Rest.swift
+++ imageCollection/Utils/Rest.swift
@@ -106,7 +106,6 @@ class Rest {
                                         case .success(let value):
                                             print("GIT IS OK: ", value)
                                         }
-                                        print("GIF RESPONSE: ", response.result.value, "@ENDS")
         }
     }
     
